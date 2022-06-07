3393daade77f77a974ce7c223437e34a6cdee47a
LeoMortari <leo.mortari.forinn@gmail.com>
2022-06-06 23:39:51 -0300

    Adiciona CORS ao projeto

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -1,14 +1,24 @@
-import express from "express";
-import consign from "consign";
 import bodyParser from "body-parser";
+import consign from "consign";
+import cors from "cors";
+import express from "express";
+
+//      CORS config, caso necessário utilizar:
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200
+// }
 
 export default () => {
   const app = express();
 
   //Configuração da API
-  // Ao usar o Potman, use o x-www-form-urlencoded
+  // Ao usar o Postman, use o x-www-form-urlencoded como opção da request
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+
+  //Habilitando o uso do CORS para a API
+  app.use(cors());
 
   //Mapeando a Controller
   consign().include("src/controller").into(app);
