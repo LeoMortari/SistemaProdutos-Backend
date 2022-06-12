115f876bee5c318b2bacccbc75d097396a7ef3f0
Lucas Kenzo <kenzoawane@gmail.com>
2022-06-12 13:16:37 -0300

    add controller venda

---
+++ src/controller/vendas/index.js
@@ -0,0 +1,37 @@
+import vendas from "../../tables/vendas";
+const rota = '/vendas';
+//cadastrar venda
+module.exports = (app) =>{
+    app.post(`${rota}/adicionar`,(req, res) => {
+      let {
+        body: {id_usuario_fk, id_pedido_fk},
+      } = req;
+
+      try {
+        if (!id_usuario_fk || !id_pedido_fk) {
+          throw new Error("Todos os campos devem ser preenchidos");
+        }
+          } 
+      catch (e) {
+        return res.status(400).send(e.message);
+      }
+      let obj = {id_usuario_fk, id_pedido_fk};
+      console.log(req.body);
+      vendas.CadastraVenda(res,obj);
+    })
+
+    app.get(`${rota}`, (req, res) => {
+      vendas.CadastrarVenda(res);
+    })
+
+    app.get(`${rota}/:id`, (req, res) => {
+      let id = parseInt(req.params.id);
+      vendas.BuscaVendaPorId(res,id)
+    })
+
+    app.patch(`${rota}/:id`, (req, res) => {
+      let valores = req.body;
+      let id = parseInt(req.params.id);
+      vendas.EditarVenda(id,valores,res);
+    })
+};
